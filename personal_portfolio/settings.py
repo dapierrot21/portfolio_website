a9129d64464e8785d3166732d694efae8438af12
Dean Pierrot <dapierrot21@gmail.com>
2020-12-09 21:40:48 -0500

    feature: blog feature blog was added to showcase my interest and knowledge of certain tech. views were created for blog index page, blog detail, also allow readers to leaViews were added to show blog details and allowing users to comment on blog post

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
 
 
 # Internationalization
